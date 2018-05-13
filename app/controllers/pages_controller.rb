6716006a577ebe42aad310f1548991caeb19125b
calapsss <charlescalapini@gmail.com>
2018-05-13 20:40:09 +0800

    Fix all bugs in posts rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
 
-	def index
-	  @posts = Post.limit(5)
-	end
+  def index
+    @life_posts = Post.by_branch('life').limit(8)
+    @physical_posts = Post.by_branch('physical').limit(8)
+    @applied_posts = Post.by_branch('applied').limit(8)
+  end
 
 	
 end
