39742d096316322369fdb72d2c72265b51a21b9e
calapsss <charlescalapini@gmail.com>
2018-05-13 15:48:05 +0800

    Create branch pages for specific posts - Inside the PostsController define hobby, study and team actions.   Define a posts_for_branch method and call it inside these actions - Add will_paginate gem - Create a _branch.html.erb partial file - Create a _create_new_post.html.erb partial file - Define a create_new_post_partial_path helper method - Create a _signed_in.html.erb partial file - Create a _not_signed_in.html.erb partial file - Create a _categories.html.erb partial file - Define a all_categories_button_partial_path helper method - Create a _all_selected.html.erb partial file - Create a _all_not_selected.html.erb partial file - Define a no_posts_partial_path helper method - Create a _no_posts.html.erb partial file - Create a hobby.html.erb template file - Create a study.html.erb template file - Create a team.html.erb template file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,28 @@ class PostsController < ApplicationController
 	def show
     @post = Post.find(params[:id])
   	end
+
+  	  def life
+    posts_for_branch(params[:action])
+  end
+
+  def physical
+    posts_for_branch(params[:action])
+  end
+
+  def applied
+    posts_for_branch(params[:action])
+  end
+
+ private
+
+	def posts_for_branch(branch)
+	  @categories = Category.where(branch: branch)
+	  @posts = get_posts.paginate(page: params[:page])
+	end
+
+	def get_posts
+	  Post.limit(30)
+	end
+
 end
