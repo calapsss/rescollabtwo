908c6d635e7d6e34de6f869e8c63c0532f053ba6
calapsss <charlescalapini@gmail.com>
2018-05-13 23:58:41 +0800

    Create UI to make new posts - Define new and create actions Define redirects if signed in or not

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,26 @@
 class PostsController < ApplicationController
+
+  before_action :redirect_if_not_signed_in, only: [:new]
+
+
 	def show
     @post = Post.find(params[:id])
   	end
+
+ def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
 
   def life
     posts_for_branch(params[:action])
@@ -44,7 +63,10 @@ class PostsController < ApplicationController
   end
 
 
-
+  def post_params
+  params.require(:post).permit(:content, :title, :category_id)
+                       .merge(user_id: current_user.id)
+end
 
 
 
